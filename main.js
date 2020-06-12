ae8fbdf91166d3db459c31595684273bf687a4a3
Shubham Sharma <shubhamsharma1172@gmail.com>
2020-06-12 18:39:33 +0530

    sticky header
    Signed-off-by: Shubham Sharma <shubhamsharma1172@gmail.com>

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-// register service worker
-console.log("ASD")
-if ('serviceWorker' in navigator) {
-    console.log("DOING1")
-    window.addEventListener("load", function () {
-        console.log("DOING")
+window.addEventListener("load", function () {
+    // register service worker
+    if ('serviceWorker' in navigator) {
         navigator.serviceWorker
             .register('/serviceWorker.js')
             .then(_ => console.log("service worker registered"))
             .catch(err => console.log("service worker not registered", err))
-    })
-}
+    }
+})
